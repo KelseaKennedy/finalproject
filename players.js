ed3cdeca028ebd4bd4f261e1c58b91906c5d3fa9
Kelsea <kkennedy27@mail.csuchico.edu>
2016-12-10 21:06:03 +0000

    added all files

---
+++ players.js
@@ -0,0 +1,17 @@
+// JavaScript File
+function checkPitcher() {
+    
+var pitchers = ["Madison Bumgarner", "Matt Moore", "Jeff Samardzija","Johnny Cueto"];
+pitchers = pitchers.toLowerCase();
+return pitchers.search() >= 0;
+
+    
+}
+
+
+
+var catchers = ["Buster Posey", "Trevor Brown"];
+var secondBase = ["Joe Panik", "Kelby Tomlinson"];
+var leftField = ["Gregor Blanco", "Angel Pagan"];
+var centerField = ["Gorkys Hernandez", "Denard Span"];
+
